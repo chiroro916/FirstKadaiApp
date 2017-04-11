8f639ecd3beabfe54ad7a189ba237bdd3f820485
Chihiro <Chihiro@M.local>
2017-04-11 22:10:58 +0900

    ラベルの追加と背景色の変更。

--- FirstKadaiApp/ViewController.swift
+++ FirstKadaiApp/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // ->背景色を空色に変更する
+        self.view.backgroundColor = UIColor.cyanColor()
     }
 
     override func didReceiveMemoryWarning() {
